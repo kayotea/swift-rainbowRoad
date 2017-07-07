548b76aaaa1104a42bcfbc10ffefc440221c3569
Placoderm <placoderm@Sarah-MacBook-Air.local>
2017-07-07 11:05:20 -0700

    initial commit

--- rainbowroad/ViewController.swift
+++ rainbowroad/ViewController.swift
@@ -7,12 +7,33 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
-
+//UIColor(red: (1.0), green: (0.5), blue: (0.0), alpha: 1.0)
+class ViewController: UIViewController, UITableViewDelegate {
+    let colors = [
+                //red
+                UIColor(red: (244.0/255.0), green: (66.0/255.0), blue: (66.0/255.0), alpha: 1.0),
+                //orange
+                UIColor(red: (244.0/255.0), green: (143.0/255.0), blue: (66.0/255.0), alpha: 1.0),
+                //yellow
+                UIColor(red: (244.0/255.0), green: (223.0/255.0), blue: (66.0/255.0), alpha: 1.0),
+                //green
+                UIColor(red: (119.0/255.0), green: (244.0/255.0), blue: (66.0/255.0), alpha: 1.0),
+                //blue
+                UIColor(red: (66.0/255.0), green: (131.0/255.0), blue: (244/255.0), alpha: 1.0),
+                //purple
+                UIColor(red: (146.0/255.0), green: (66.0/255.0), blue: (244.0/255.0), alpha: 1.0)]
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    //func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        //cell.contentView.backgroundColor = colors[indexPath.row]
+    //}
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,3 +44,26 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return colors.count
+    }
+    //func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> Int {
+    //    return cell_height
+    //}
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
+        //cell.height? =
+        cell.backgroundColor = self.colors[indexPath.row]
+        return cell
+    }
+    //func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+    //    print("here")
+    //    cell.contentView.backgroundColor = colors[indexPath.row]
+    //}
+    //func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    //    let cell = tableView.dequeueReusableCellWithIdentifier("MyCell", forIndexPath: indexPath)
+        
+    //    cell.tex
+    //}
+}
